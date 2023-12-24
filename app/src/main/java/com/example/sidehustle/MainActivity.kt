2425a24253919f144eadbe4f654b8377af41f914
YeeJing <ganyeejing@gmail.com>
2023-12-25 01:12:53 +0800

    Employee My Jobs Activity UI First Commit

--- app/src/main/java/com/example/sidehustle/MainActivity.kt
+++ app/src/main/java/com/example/sidehustle/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.sidehustle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.my_butt).setOnClickListener {
+            toEmployeeHome(it)
+        }
+    }
+
+    fun toEmployeeHome(view: View){
+        val context = view.context
+        val intent = Intent(context,EmployerMyJobsActivity::class.java)
+
+        context.startActivity(intent)
     }
 }
