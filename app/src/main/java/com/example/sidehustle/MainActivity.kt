f933467aedc34c9a56cb45e50b81243687a27dee
YeeJing <ganyeejing@gmail.com>
2023-12-25 01:57:24 +0800

    Employee My Jobs Activity UI Second Commit

--- app/src/main/java/com/example/sidehustle/MainActivity.kt
+++ app/src/main/java/com/example/sidehustle/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.sidehustle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
