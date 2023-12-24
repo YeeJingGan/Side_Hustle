dfdd9fcf5df010494a01ac77524aaf61c3175e3f
JieShen <yeapjieshen@hotmail.com>
2023-12-25 00:35:48 +0800

    Commit by JieShen at 12:35 AM 25 Dec 23

---
+++ app/src/main/java/com/example/sidehustle/EmployeeHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sidehustle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployeeHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_home)
+    }
+}
--- app/src/main/java/com/example/sidehustle/MainActivity.kt
+++ app/src/main/java/com/example/sidehustle/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.sidehustle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            toEmployeeHome(it)
+        }
+    }
+
+    fun toEmployeeHome(view: View){
+        val context = view.context
+        val intent = Intent(context,EmployeeHomeActivity::class.java)
+
+        context.startActivity(intent)
     }
 }
