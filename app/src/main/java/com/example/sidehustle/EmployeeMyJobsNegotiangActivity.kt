4d1a2d5ad3b0164e9343fbc5767e4f741195685d
YeeJing <ganyeejing@gmail.com>
2023-12-27 02:03:31 +0800

    Left jobs details
    27/12/23 2:03 am

--- app/src/main/java/com/example/sidehustle/EmployeeMyJobsNegotiangActivity.kt
+++ app/src/main/java/com/example/sidehustle/EmployeeMyJobsNegotiangActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class EmployeeMyJobsNegotiangActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_employee_my_jobs_negoting)
+        setContentView(R.layout.activity_employer_my_jobs_negotiating)
     }
 }
