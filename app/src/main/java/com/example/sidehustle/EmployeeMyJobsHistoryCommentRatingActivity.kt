cb41d8463c20ced2e38dd7ea896b0af6f7a0b72a
YeeJing <ganyeejing@gmail.com>
2023-12-31 18:06:28 +0800

    done all UI
    31/12/23 6:05PM

---
+++ app/src/main/java/com/example/sidehustle/EmployeeMyJobsHistoryCommentRatingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sidehustle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployeeMyJobsHistoryCommentRatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_my_jobs_history_comment_rating)
+    }
+}
---
+++ app/src/main/java/com/example/sidehustle/EmployeeMyJobsNegotiatingWaitingReplyJosDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sidehustle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployeeMyJobsNegotiatingWaitingReplyJosDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_my_jobs_negotiating_waiting_reply_jos_details)
+    }
+}
