497c07b810577c27ec046e4713dcfe5e3d7ec4a8
YeeJing <ganyeejing@gmail.com>
2024-01-02 00:30:59 +0800

    commit_2
    2/1/24 12.30am

---
+++ app/src/main/java/com/example/sidehustle/EmployerMyJobsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sidehustle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.ViewPager
+import com.example.sidehustle.databinding.ActivityEmployerMyJobsBinding
+import com.google.android.material.tabs.TabLayout
+
+class EmployerMyJobsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEmployerMyJobsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_employer_my_jobs)
+
+        var viewPager = binding.employerMyJobsViewPager as ViewPager
+        var tabLayout = binding.employerMyJobsOngoingTabLayout as TabLayout
+
+        val fragmentAdapter = EmployeeMyJobsFragmentAdapter(supportFragmentManager)
+        fragmentAdapter.addFragment(EmployerOngoingFragment(),"Ongoing")
+        fragmentAdapter.addFragment(EmployerNegotiatingFragment(),"Negotiating")
+        fragmentAdapter.addFragment(EmployerOngoingFragment(),"History")
+
+        viewPager.adapter = fragmentAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
