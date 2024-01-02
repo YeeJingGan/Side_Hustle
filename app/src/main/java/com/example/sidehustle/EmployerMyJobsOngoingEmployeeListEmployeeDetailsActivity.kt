c1967f316b27af44564e6bd2326ca467181f917c
YeeJing <ganyeejing@gmail.com>
2024-01-02 11:30:51 +0800

    commit_4
    2/1/24 11.30am

--- app/src/main/java/com/example/sidehustle/EmployerMyJobsOngoingEmployeeListEmployeeDetailsActivity.kt
+++ app/src/main/java/com/example/sidehustle/EmployerMyJobsOngoingEmployeeListEmployeeDetailsActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sidehustle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class EmployerMyJobsOngoingEmployeeListEmployeeDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employer_my_jobs_ongoing_employee_list_employee_details)
+
+        val intent = intent
+
+        val employeeID = intent.getLongExtra("employeeID",-100)
+        Log.i("INTENT",employeeID.toString())
     }
 }
