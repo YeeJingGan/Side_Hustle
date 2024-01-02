ea93140e4e9fe0e75bc10c298687a2eb873f0a65
YeeJing <ganyeejing@gmail.com>
2024-01-02 11:30:51 +0800

    commit_4
    2/1/24 11.30am

--- app/src/main/java/com/example/sidehustle/EmployerOngoingEmployeeListAdapter.kt
+++ app/src/main/java/com/example/sidehustle/EmployerOngoingEmployeeListAdapter.kt
@@ -7,45 +7,46 @@ import android.widget.Filterable
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sidehustle.databinding.FragmentEmployerHomeApprovedJobsBinding
+import com.example.sidehustle.databinding.FragmentEmployerMyJobsOngoingEmployeeListBinding
 
-class EmployerOngoingEmployeeListAdapter(private val allEmployees: List<EmployerOngoingEmployeeListData>):
-    RecyclerView.Adapter<EmployerOngoingEmployeeListAdapter.ViewHolder> (),Filterable{
+class EmployerOngoingEmployeeListAdapter(private val allEmployees: List<EmployeeEntity>) :
+    RecyclerView.Adapter<EmployerOngoingEmployeeListAdapter.ViewHolder>() {
 
-    private var employees: List<EmployerOngoingEmployeeListData> = allEmployees
 
-    inner class ViewHolder(private val binding: Fragment)
+    inner class ViewHolder(private val binding: FragmentEmployerMyJobsOngoingEmployeeListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(employee: EmployeeEntity) {
+            binding.employee = employee
+            // TODO : PUT EXTRA PASS DATA TO DESTINATION ACTIVITY
+            binding.employerMyJobsOngoingEmployeeListLisitemButton.setOnClickListener {
+                val intent = Intent(
+                    it.context,
+                    EmployerMyJobsOngoingEmployeeListEmployeeDetailsActivity::class.java
+                )
+                intent.putExtra("employeeID", employee.employeeID)
+                it.context.startActivity(intent)
+            }
+            binding.executePendingBindings()
+        }
+    }
 
-//    inner class ViewHolder(private val binding: FragmentEmployerHomeApprovedJobsBinding) :
-//        RecyclerView.ViewHolder(binding.root) {
-//
-//        fun bind(job: JobEntity) {
-//            binding.job = job
-//            // TODO : PUT EXTRA PASS DATA TO DESTINATION ACTIVITY
-//            binding.employerHomeApprovedJobsViewDetailsButton.setOnClickListener {
-//                val intent = Intent(it.context, EmployerHomeJobDetailsActivity::class.java)
-//                intent.putExtra("jobID", job.jobID)
-//                it.context.startActivity(intent)
-//            }
-//            binding.executePendingBindings()
-//        }
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return filteredJobs.size
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        holder.bind(filteredJobs[position])
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val inflater = LayoutInflater.from(parent.context)
-//        val binding: FragmentEmployerHomeApprovedJobsBinding = DataBindingUtil.inflate(
-//            inflater,
-//            R.layout.fragment_employer_home_approved_jobs,
-//            parent,
-//            false
-//        )
-//        return ViewHolder(binding)
-//    }
+
+    override fun getItemCount(): Int {
+        return allEmployees.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(allEmployees[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding:  FragmentEmployerMyJobsOngoingEmployeeListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_employer_my_jobs_ongoing_employee_list,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
 }
--- app/src/main/java/com/example/sidehustle/EmployerOngoingEmployeeListData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sidehustle
-
-data class EmployerOngoingEmployeeListData(
-    val employeeID : Long,
-    val employeeName : String,
-    val employeeWages : Int,
-    val employeeRating : Int,
-    val employeePic : String //path to pic
-)
