83f6eeca26359a0cdb70a733f637d84c1f755234
YeeJing <ganyeejing@gmail.com>
2024-01-04 23:04:53 +0800

    last commit bye bye
    4/1/24 9.43pm

--- app/src/main/java/com/example/sidehustle/EmployeeMyProfileActivity.kt
+++ app/src/main/java/com/example/sidehustle/EmployeeMyProfileActivity.kt
@@ -1,60 +1,27 @@
 package com.example.sidehustle
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import androidx.viewpager2.widget.ViewPager2
-import com.example.sidehustle.databinding.ActivityEmployeeMyProfileBinding
-import com.example.sidehustle.databinding.ActivityEmployerMyProfileBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.launch
 
 class EmployeeMyProfileActivity : AppCompatActivity() {
-//    lateinit var viewPager2: ViewPager2
-    lateinit var adapter: EmployeeMyProfileAdapter
-    lateinit var binding : ActivityEmployeeMyProfileBinding
-    lateinit var viewModel:EmployeeMyProfileViewModel
 
+//    lateinit var viewPager2: ViewPager2
+//    lateinit var adapter: EmployeeMyProfileReviewsCommentsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_employee_my_profile)
+        setContentView(R.layout.activity_employee_my_profile)
 
+        val reviews = createReviewsCommentsList()
 
-        viewModel = ViewModelProvider(
-            this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-        ).get(EmployeeMyProfileViewModel::class.java)
+//        viewPager2 = findViewById(R.id.my_profile_viewpager2)
+//        adapter = EmployeeMyProfileReviewsCommentsAdapter(reviews)
 
-
-//        viewPager2 = binding.myProfileViewpager2
-//        viewPager2.adapter = EmployeeMyProfileAdapter(emptyList(),viewModel)
-
-        viewModel.viewModelScope.launch {
-            val ratings = viewModel.getRatingsForEmployer(1,"EMPLOYEE")
-            adapter = EmployeeMyProfileAdapter(ratings,viewModel)
-//            viewPager2.adapter = adapter
-
-//            if(ratings.size < 2){
-//                binding.myProfileNextReviewButton.visibility = View.INVISIBLE
-//            }
-
-            val employee = viewModel.getEmployeeByEmployeeID(1)
-
-            binding.employee = employee
-        }
-
-        viewModel.getAverageRatingByJobIDAndCommenter(1,"EMPLOYEE")
-
-        viewModel.starCount.observe(this) {
-            updateStarColors(it)
-        }
-        setListeners()
+//        viewPager2.adapter = adapter
 
         setListeners()
 
@@ -91,30 +58,11 @@ class EmployeeMyProfileActivity : AppCompatActivity() {
         }
     }
 
-    private fun updateStarColors(starsCount: Int) {
-        val stars = arrayOf(
-            binding.myProfileStar1,
-            binding.myProfileStar2,
-            binding.myProfileStar3,
-            binding.myProfileStar4,
-            binding.myProfileStar5
-        )
-
-        for (i in 0..<stars.size) {
-            if (i < starsCount) {
-                stars[i].setImageResource(R.drawable.ic_star_24px)
-                stars[i].setColorFilter(Color.parseColor("#FDB915"))
-            } else {
-                stars[i].setImageResource(R.drawable.ic_star_hollow_24px)
-                stars[i].setColorFilter(Color.parseColor("#000000"))
-            }
-        }
-    }
-
     private fun setListeners() {
         findViewById<ImageButton>(R.id.my_profile_settings_icon).setOnClickListener {
             toAnotherActivity(it, EmployeeMyProfileSettingsActivity::class.java)
         }
+
 
 //        val previousButton = findViewById<ImageButton>(R.id.my_profile_previous_review_button)
 //        val nextButton = findViewById<ImageButton>(R.id.my_profile_next_review_button)
